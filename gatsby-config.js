07e4a0117f99255da492b1585709219c530f33a2
Maciej Leszczyński <mml@Maciejs-MacBook-Pro.local>
2018-08-27 13:20:22 +0200

    Add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,33 @@ module.exports = {
     'gatsby-plugin-react-next',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-emotion',
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+    //     // Puts tracking script in the head instead of the body
+    //     // head: false,
+    //     // Setting this parameter is optional
+    //     // anonymize: true,
+    //     // Setting this parameter is also optional
+    //     // respectDNT: true,
+    //     // Avoids sending pageview hits from custom paths
+    //     // exclude: ["/preview/**", "/do-not-track/me/too/"],
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-plugin-google-tagmanager`,
+    //   options: {
+    //     id: "YOUR_GOOGLE_TAGMANAGER_ID",
+
+    //     // Include GTM in development.
+    //     // Defaults to false meaning GTM will only be loaded in production.
+    //     includeInDevelopment: false,
+
+    //     // Specify optional GTM environment details.
+    //     gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+    //     gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+    //   },
+    // },
   ],
 }
